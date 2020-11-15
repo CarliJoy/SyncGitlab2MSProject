3f2517099420ada7034f4da2c4f9676a1e29570d
Carli* Freudenberg <kound@posteo.de>
2020-11-15 01:20:02 +0100

    use read the doc sphinx style

--- docs/conf.py
+++ docs/conf.py
@@ -78,8 +78,7 @@ except Exception as e:
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.todo',
               'sphinx.ext.autosummary', 'sphinx.ext.viewcode', 'sphinx.ext.coverage',
               'sphinx.ext.doctest', 'sphinx.ext.ifconfig', 'sphinx.ext.mathjax',
-              'sphinx.ext.napoleon']
-extensions.append('recommonmark')
+              'sphinx.ext.napoleon', "sphinx_rtd_theme",  'recommonmark' ]
 
 
 # Add any paths that contain templates here, relative to this directory.
@@ -161,7 +160,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'alabaster'
+html_theme = "sphinx_rtd_theme"
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
