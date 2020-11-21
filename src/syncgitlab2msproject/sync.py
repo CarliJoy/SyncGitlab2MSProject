0a67f485d7b79f5307715b1bd4bd742a0ceaefae
Carli <Carl.Freudenberg@enercon.de>
2020-11-21 16:42:53 +0100

    remove empty hyperlink bug

--- src/syncgitlab2msproject/sync.py
+++ src/syncgitlab2msproject/sync.py
@@ -59,13 +59,19 @@ def get_weburl_from_task(task: Optional[Task], gitlab_url: WebURL) -> Optional[W
     """
     Get the weburl from MS Project Task (is saved as hyperlink)
     """
+
+    def check_get_url(value: Optional[str]) -> Optional[WebURL]:
+        if value is not None:
+            check_url = WebURL(value)
+            if is_gitlab_hyperlink(check_url, gitlab_url):
+                return check_url
+        return None
+
     if task is not None:
-        if (url := task.hyperlink_address) is not None:
-            url = WebURL(url)
+        if (url := check_get_url(task.hyperlink_address)) is not None:
+            return url
         # If not as hyperlink we also look in task.text29 field
-        elif (url := task.text29) is not None:
-            url = WebURL(url)
-        if is_gitlab_hyperlink(url, gitlab_url):
+        if (url := check_get_url(task.text29)) is not None:
             return url
     return None
 
@@ -256,7 +262,10 @@ def sync_gitlab_issues_to_ms_project(
         if ref_issue is not None:
             logger.info(f"Syncing {ref_issue} into {task}")
             synced += update_task_with_issue_data(task, ref_issue)
-
+        else:
+            logger.info(
+                f"Not Syncing {task} as a not reference to an gitlab issue could be found"
+            )
     # adding everything that was not synced and is not duplicate
     for ref_id in non_moved:
         if ref_id not in synced:
